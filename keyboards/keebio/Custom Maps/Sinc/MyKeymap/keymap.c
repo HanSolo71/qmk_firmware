198b061eb951e21ba269e2bcaa997da97f20656d
HanSolo71 <HanSolo552@gmail.com>
2022-11-07 13:53:16 -0500

    Update Sinc

---
+++ keyboards/keebio/Custom Maps/Sinc/MyKeymap/keymap.c
@@ -0,0 +1,100 @@
+#include QMK_KEYBOARD_H
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+#define _BASE 0
+#define _FN1 1
+
+bool is_alt_tab_active = false; // ADD this near the begining of keymap.c
+bool is_alt_shift_tab_active = false; // ADD this near the begining of keymap.c
+uint16_t alt_tab_timer = 0;     // we will be using them soon.
+
+//ALT TAB Encoder Timer
+void matrix_scan_user(void) { // The very important timer.
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      unregister_code(KC_LSHIFT);
+      is_alt_tab_active = false;
+      is_alt_shift_tab_active = false;
+    }
+  }
+};
+
+
+
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  ALT_TAB,
+  ALT_SHIFT_TAB,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_65_with_macro(
+    KC_MEDIA_PLAY_PAUSE,   KC_F5,   KC_ESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DEL,  KC_BSPC, KC_AUDIO_MUTE,
+    KC_F9,   KC_F10,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
+    KC_F11,   KC_F12,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
+    KC_GRAVE,   KC_DEL,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    DYN_MACRO_PLAY1,   KC_PSCR,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           MO(_FN1),KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+  ),
+
+  [_FN1] = LAYOUT_65_with_macro(
+    _______, _______, KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, KC_ENT,
+    RGB_HUI, RGB_HUD, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    RGB_SAI, RGB_SAD, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    RGB_VAI, RGB_VAD, KC_LSFT, KC_GRAVE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    DYN_REC_START1, DYN_REC_STOP, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+  )
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (get_highest_layer(layer_state|default_layer_state) == 0) {
+        if (index == 0) {
+            if (clockwise) {
+                tap_code(KC_MEDIA_NEXT_TRACK);
+            } else {
+                tap_code(KC_MEDIA_PREV_TRACK);
+            }
+        } else if (index == 1) {
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
+        }
+    }
+
+    else if (get_highest_layer(layer_state|default_layer_state) == 1) {  /* Layer 1 */
+        if (index == 0) {
+            if (clockwise) {
+                rgblight_increase_hue();
+            } else {
+                rgblight_decrease_hue();
+            }
+        } else if (index == 1) {
+            if (clockwise) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                unregister_code(KC_LSHIFT);
+                register_code(KC_LALT);
+                }
+                alt_tab_timer = timer_read();
+                tap_code(KC_TAB);
+            } else {
+                if (!is_alt_shift_tab_active) {
+                    is_alt_shift_tab_active = true;
+                register_code(KC_LALT);
+                register_code(KC_LSHIFT);
+                }
+                alt_tab_timer = timer_read();
+                tap_code(KC_TAB);
+            }
+        }
+    }
+    return false;
+};
