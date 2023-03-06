d9c984f937e4274aaf7a3ce1da9e449d839d475a
HanSolo71 <HanSolo552@gmail.com>
2023-03-06 13:35:51 -0500

    Update keymap.c

--- keyboards/keebio/sinc/keymaps/MyKeymap/keymap.c
+++ keyboards/keebio/sinc/keymaps/MyKeymap/keymap.c
@@ -18,7 +18,7 @@ void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active) {
     if (timer_elapsed(alt_tab_timer) > 1000) {
       unregister_code(KC_LALT);
-      unregister_code(KC_LSHIFT);
+      unregister_code(KC_LSFT);
       is_alt_tab_active = false;
       is_alt_shift_tab_active = false;
     }
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [1] = LAYOUT_80_with_macro(
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    RGB_HUI, RGB_HUD, KC_GESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
+    RGB_HUI, RGB_HUD, KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______,
     RGB_SAI, RGB_SAD, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     RGB_VAI, RGB_VAD, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    DYN_REC_START1, DYN_REC_STOP, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   ),
 };
 
@@ -81,7 +81,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             if (clockwise) {
                 if (!is_alt_tab_active) {
                     is_alt_tab_active = true;
-                unregister_code(KC_LSHIFT);
+                unregister_code(KC_LSFT);
                 register_code(KC_LALT);
                 }
                 alt_tab_timer = timer_read();
@@ -90,7 +90,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 if (!is_alt_shift_tab_active) {
                     is_alt_shift_tab_active = true;
                 register_code(KC_LALT);
-                register_code(KC_LSHIFT);
+                register_code(KC_LSFT);
                 }
                 alt_tab_timer = timer_read();
                 tap_code(KC_TAB);
