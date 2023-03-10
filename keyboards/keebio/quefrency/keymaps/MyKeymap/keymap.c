ec8d63b6428aa039f546d336b74dffa9b253dd26
HanSolo71 <HanSolo552@gmail.com>
2023-03-09 19:50:52 -0500

    words

--- keyboards/keebio/quefrency/keymaps/MyKeymap/keymap.c
+++ keyboards/keebio/quefrency/keymaps/MyKeymap/keymap.c
@@ -18,7 +18,7 @@ void matrix_scan_user(void) { // The very important timer.
   if (is_alt_tab_active) {
     if (timer_elapsed(alt_tab_timer) > 1000) {
       unregister_code(KC_LALT);
-      unregister_code(KC_LSHIFT);
+      unregister_code(KC_LSFT);
       is_alt_tab_active = false;
       is_alt_shift_tab_active = false;
     }
@@ -40,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F9,   KC_F10,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
     KC_F11,   KC_F12,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
     KC_GRAVE,   KC_DEL,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    DYN_MACRO_PLAY1,   KC_PSCR,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           MO(_FN1),KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    _______,   KC_PSCR,  KC_LCTL, KC_LGUI, KC_LALT, MO(_FN1),KC_SPC,           MO(_FN1),KC_SPC,  KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_FN1] = LAYOUT_65_with_macro(
     _______, _______, KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, KC_ENT,
     RGB_HUI, RGB_HUD, RGB_TOG, RGB_MOD, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    RGB_SAI, RGB_SAD, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    RGB_SAI, RGB_SAD, RGB_MODE_PLAIN, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     RGB_VAI, RGB_VAD, KC_LSFT, KC_GRAVE, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    DYN_REC_START1, DYN_REC_STOP, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
 
@@ -80,7 +80,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             if (clockwise) {
                 if (!is_alt_tab_active) {
                     is_alt_tab_active = true;
-                unregister_code(KC_LSHIFT);
+                unregister_code(KC_LSFT);
                 register_code(KC_LALT);
                 }
                 alt_tab_timer = timer_read();
@@ -89,7 +89,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
                 if (!is_alt_shift_tab_active) {
                     is_alt_shift_tab_active = true;
                 register_code(KC_LALT);
-                register_code(KC_LSHIFT);
+                register_code(KC_LSFT);
                 }
                 alt_tab_timer = timer_read();
                 tap_code(KC_TAB);
